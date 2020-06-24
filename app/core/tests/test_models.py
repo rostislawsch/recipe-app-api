b0b9fb01114caecc2854a1e1a04ff862e158cec8
Rostislaw Schmalz <rostislawschmalz@gmx.de>
2020-06-24 15:35:40 +0200

    lint code

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 # wir importieren das, damit wir später das user model ändern können
 
+
 class ModelTests(TestCase):
-    
+
     # wir testen, ob unsere create user function einen neuen user kreiert
     def test_create_user_with_email_successful(self):
         """ Test creating a new user with an email is successful """
