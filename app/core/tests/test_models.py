f1a5f620ee54b805b2f27d02844ed361144709ec
Rostislaw Schmalz <rostislawschmalz@gmx.de>
2020-06-24 15:30:55 +0200

    code lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,17 +3,21 @@ from django.contrib.auth import get_user_model
 # wir importieren das, damit wir später das user model ändern können
 
 class ModelTests(TestCase):
+    
     # wir testen, ob unsere create user function einen neuen user kreiert
     def test_create_user_with_email_successful(self):
         """ Test creating a new user with an email is successful """
         email = 'test@rostislaw.com'
         password = "Testpass1234"
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
-        
+
+
         self.assertEqual(user.email, email)
-        # check_password() is a helper function that comes with the django user model.
+        # check_password() is a helper function that comes
+        # with the django user model.
         # it returns true, if the passwprd is correct and false if not
         self.assertTrue(user.check_password(password))
+        
