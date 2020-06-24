d8f2d276b22f24cf9668db55a4e93ecfba828f67
Rostislaw Schmalz <rostislawschmalz@gmx.de>
2020-06-24 11:52:42 +0200

    lint code

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,8 +5,9 @@ from app.calc import add, subtract
 class CalcTests(TestCase):
     """ Test that two numbersare added together """
     def test_add_numbers(self):
-        self.assertEqual(add(3,8),11)
+        self.assertEqual(add(3, 8), 11)
     
     def test_subtract_numbers(self):
         """ Test that values are subtracted and returned"""
-        self.assertEqual(subtract(4,5),-1)
+        self.assertEqual(subtract(4, 5), -1)
+        
