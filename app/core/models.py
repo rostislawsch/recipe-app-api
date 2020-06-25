949d700b073481fe8eee03f2080f936a5b09e6e0
Rostislaw Schmalz <rostislawschmalz@gmx.de>
2020-06-25 23:25:25 +0200

    fix error in createsuperuser

--- app/core/models.py
+++ app/core/models.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(self._db)
+        user.save(using=self._db)
         return user
 
 
